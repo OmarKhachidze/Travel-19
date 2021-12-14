44daa3012680948b69629a07db26e7cbe24c6ad4
Omar Khachidze <omar.khachidze.1@btu.edu.ge>
2021-12-14 22:33:54 +0400

    add basic splash screen for demonstration

--- app/src/main/java/ge/bootcamp/travel19/ui/activity/MainActivity.kt
+++ app/src/main/java/ge/bootcamp/travel19/ui/activity/MainActivity.kt
@@ -1,14 +1,102 @@
 package ge.bootcamp.travel19.ui.activity
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.activity.viewModels
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import ge.bootcamp.travel19.R
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var splashScreen: SplashScreen
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        splashScreen = installSplashScreen()
         setContentView(R.layout.activity_main)
+        customizeSplashScreen(splashScreen)
+
     }
+
+    private fun customizeSplashScreen(splashScreen: SplashScreen) {
+         keepSplashScreenLonger(splashScreen)
+        customizeSplashScreenExit(splashScreen)
+    }
+
+    private fun customizeSplashScreenExit(splashScreen: SplashScreen) {
+        splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
+
+            showSplashExitAnimator(splashScreenViewProvider.view) {
+                splashScreenViewProvider.remove()
+            }
+
+            showSplashIconExitAnimator(splashScreenViewProvider.iconView) {
+                splashScreenViewProvider.remove()
+            }
+        }
+    }
+
+    private fun keepSplashScreenLonger(splashScreen: SplashScreen) {
+        splashScreen.setKeepVisibleCondition { !viewModel.isDataReady() }
+    }
+
+    private fun showSplashExitAnimator(splashScreenView: View, onExit: () -> Unit = {}) {
+
+        val scaleOut = ObjectAnimator.ofFloat(
+            splashScreenView,
+            View.SCALE_X,
+            View.SCALE_Y,
+            Path().apply {
+                moveTo(1.0f, 1.0f)
+                lineTo(0f, 0f)
+            }
+        )
+        AnimatorSet().run {
+            duration = resources.getInteger(R.integer.splash_exit_total_duration).toLong()
+            interpolator = AnticipateInterpolator()
+            playTogether(scaleOut)
+
+            doOnEnd {
+                onExit()
+            }
+            start()
+        }
+    }
+
+
+    private fun showSplashIconExitAnimator(iconView: View, onExit: () -> Unit = {}) {
+
+        val scaleOut = ObjectAnimator.ofFloat(
+            iconView,
+            View.SCALE_X,
+            View.SCALE_Y,
+            Path().apply {
+                moveTo(1.0f, 1.0f)
+                lineTo(0.3f, 0.3f)
+            }
+        )
+
+        AnimatorSet().run {
+            interpolator = AnticipateInterpolator()
+            duration = resources.getInteger(R.integer.splash_exit_total_duration).toLong()
+
+            playTogether(scaleOut)
+            doOnEnd {
+                onExit()
+            }
+            start()
+        }
+    }
+
 }
