cc277d550eaed7f9c6dd9d920d82e17a9a86cf63
likachedia <likachedia@gmail.com>
2021-12-17 21:39:32 +0400

    feat: add bottom app bar

--- app/src/main/java/ge/bootcamp/travel19/ui/activity/MainActivity.kt
+++ app/src/main/java/ge/bootcamp/travel19/ui/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package ge.bootcamp.travel19.ui.activity
 
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.app.Activity
 import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,22 +13,44 @@ import androidx.activity.viewModels
 import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import ge.bootcamp.travel19.R
+import ge.bootcamp.travel19.databinding.ActivityMainBinding
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     private lateinit var splashScreen: SplashScreen
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         splashScreen = installSplashScreen()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+
+        val navView: BottomNavigationView = binding.bottomNavigationView
+
+        //val navController = findNavController(R.id.nav_host_fragment)
+        navView.setupWithNavController(navController)
+        //setupWithNavController(navController)
+
+
         customizeSplashScreen(splashScreen)
-        findViewById<BottomNavigationView>(R.id.bottomNavigationView).background = null
+        findViewById<BottomNavigationView>(ge.bootcamp.travel19.R.id.bottomNavigationView).background = null
+
     }
 
     private fun customizeSplashScreen(splashScreen: SplashScreen) {
@@ -90,13 +113,37 @@ class MainActivity : AppCompatActivity() {
 
         AnimatorSet().run {
             interpolator = AnticipateInterpolator()
-            duration = resources.getInteger(R.integer.splash_exit_total_duration).toLong()
+            duration = resources.getInteger(ge.bootcamp.travel19.R.integer.splash_exit_total_duration).toLong()
 
             playTogether(scaleOut)
             doOnEnd {
                 onExit()
             }
             start()
+        }
+    }
+
+    private fun navigation() {
+        val bottomAppBar = findViewById<BottomAppBar>(R.id.bottomAppBar)
+        //set bottom bar to Action bar as it is similar like Toolbar
+        setSupportActionBar(bottomAppBar)
+        bottomAppBar.setNavigationOnClickListener {
+            it.findNavController().navigate(R.id.miSearch)
+        }
+        findViewById<BottomAppBar>(R.id.bottomAppBar).setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                ge.bootcamp.travel19.R.id.miSearch -> {
+                    // Handle search icon press
+                    findNavController(R.id.miHome).navigate(R.id.miSearch)
+                    true
+                }
+                R.id.miHome -> {
+                    // Handle more item (inside overflow menu) press
+                    //findNavController(R.id.bottomNavigationView).navigate(ge.bootcamp.travel19.R.id.homeFragment)
+                    true
+                }
+                else -> false
+            }
         }
     }
 
