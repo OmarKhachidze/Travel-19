b0d8aa05cb74fa6edea6dcda927dbbd2d9d3630c
Omar Khachidze <omar.khachidze.1@btu.edu.ge>
2021-12-30 22:33:08 +0400

    add hilt annotation to fragment

--- app/src/main/java/ge/bootcamp/travel19/ui/fragments/country_restrictions/CountryRestrictionsFragment.kt
+++ app/src/main/java/ge/bootcamp/travel19/ui/fragments/country_restrictions/CountryRestrictionsFragment.kt
@@ -1,24 +1,25 @@
 package ge.bootcamp.travel19.ui.fragments.country_restrictions
 
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
+import dagger.hilt.android.AndroidEntryPoint
 import ge.bootcamp.travel19.R
 import ge.bootcamp.travel19.databinding.FragmentCountryRestrictionsBinding
 import ge.bootcamp.travel19.extensions.gone
-import ge.bootcamp.travel19.extensions.invisible
 import ge.bootcamp.travel19.extensions.showSnack
 import ge.bootcamp.travel19.extensions.visible
 import ge.bootcamp.travel19.ui.fragments.BaseFragment
 import ge.bootcamp.travel19.utils.Resource
 import kotlinx.coroutines.flow.collect
 
-
+@AndroidEntryPoint
 class CountryRestrictionsFragment :
     BaseFragment<FragmentCountryRestrictionsBinding>(FragmentCountryRestrictionsBinding::inflate) {
     private val countryRestrictionArgs: CountryRestrictionsFragmentArgs by navArgs()
 
-    private val restrictionsViewModel: CountryRestrictionsViewModel by activityViewModels()
+    private val restrictionsViewModel: CountryRestrictionsViewModel by viewModels()
 
     override fun start() {
         countryRestrictionArgs.selectedCountry?.let {
@@ -30,10 +31,11 @@ class CountryRestrictionsFragment :
                         }
                         is Resource.Success -> {
                             binding.prCircular.gone()
-                            binding.root.showSnack(
-                                "${state.data?.data?.area?.name}",
-                                R.color.success_green
-                            )
+                            binding.tvSummary.text = state.data?.data?.summary
+//                            binding.root.showSnack(
+//                                "${state.data?.data?.area?.name}",
+//                                R.color.success_greene
+//                            )
                         }
                         is Resource.Error -> {
                             binding.root.showSnack("Error", R.color.error_red)
--- app/src/main/java/ge/bootcamp/travel19/ui/fragments/search_country/SearchCountryFragment.kt
+++ app/src/main/java/ge/bootcamp/travel19/ui/fragments/search_country/SearchCountryFragment.kt
@@ -1,16 +1,13 @@
 package ge.bootcamp.travel19.ui.fragments.search_country
 
 import android.os.Bundle
-import android.util.Log.d
-import android.view.View
-import androidx.core.view.isVisible
 import androidx.core.widget.doAfterTextChanged
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.transition.MaterialSharedAxis
+import dagger.hilt.android.AndroidEntryPoint
 import ge.bootcamp.travel19.R
 import ge.bootcamp.travel19.databinding.FragmentSearchCountryBinding
 import ge.bootcamp.travel19.extensions.gone
@@ -19,24 +16,18 @@ import ge.bootcamp.travel19.extensions.showSnack
 import ge.bootcamp.travel19.extensions.visible
 import ge.bootcamp.travel19.model.countries.Countries
 import ge.bootcamp.travel19.ui.fragments.BaseFragment
-import ge.bootcamp.travel19.ui.fragments.country_restrictions.CountryRestrictionsFragment
-import ge.bootcamp.travel19.ui.fragments.country_restrictions.CountryRestrictionsFragmentArgs
 import ge.bootcamp.travel19.ui.fragments.search_country.adapter.CountriesAdapter
 import ge.bootcamp.travel19.utils.Resource
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 
-
+@AndroidEntryPoint
 class SearchCountryFragment :
     BaseFragment<FragmentSearchCountryBinding>(FragmentSearchCountryBinding::inflate) {
 
     private val countriesAdapter: CountriesAdapter = CountriesAdapter()
-    private val countriesViewModel: SearchCountryViewModel by activityViewModels()
+    private val countriesViewModel: SearchCountryViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         applyAnim()
