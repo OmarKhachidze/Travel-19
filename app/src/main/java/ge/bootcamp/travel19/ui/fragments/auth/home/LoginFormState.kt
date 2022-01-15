eeb808351aaad955984241f788f5ae7daf8e4a48
Omar Khachidze <omar.khachidze.1@btu.edu.ge>
2022-01-16 01:08:47 +0400

    done input validations and add string extensions for validating inputs

--- app/src/main/java/ge/bootcamp/travel19/ui/fragments/auth/home/LoginFormState.kt
+++
@@ -1,8 +0,0 @@
-package ge.bootcamp.travel19.ui.fragments.auth.home
-
-
-data class LoginFormState(
-    val usernameError: Int? = null,
-    val passwordError: Int? = null,
-    val isDataValid: Boolean = false
-)
